2872f5a645fa3f8dab6f64b6878ec74cf8a880f3
Firdavsi <firdavsinurov326@gmail.com>
2023-04-10 22:37:59 +0300

    lab1 | initials-and-jump/drag-and-drop

---
+++ lab1/drag-and-drop/script.js
@@ -0,0 +1,37 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const draggable = document.getElementById('draggable')
+    const width = 205
+    const height = 200
+    draggable.style.width = width
+    draggable.style.height = height
+
+    let x= 100
+    let y= 100
+    let dX = 0
+    let dY = 0
+
+    const move = () => {
+        draggable.style.top = `${y}px`
+        draggable.style.left = `${x}px`
+    }
+
+    move()
+    
+    draggable.addEventListener('dragstart', (event) => {
+        setTimeout(() => {
+            draggable.classList.add('hide')
+        }, 0)
+
+        dX = event.clientX - x;
+        dY = event.clientY - y;
+    })
+
+    draggable.addEventListener('dragend', (event) => {
+        draggable.classList.remove('hide')
+        const clientY = event.clientY < 0 ? 0 : event.clientY;
+        const clientX = event.clientX < 0 ? 0 : event.clientX;
+        y = clientY - dY
+        x = clientX - dX
+        move()
+    })
+})
